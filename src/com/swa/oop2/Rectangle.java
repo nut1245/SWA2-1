a8a79016f207070826120b4b3313d117595ca7a9
nut1245 <73591319+nut1245@users.noreply.github.com>
2021-07-12 17:37:55 +0700

    Initial commit

---
+++ src/com/swa/oop2/Rectangle.java
@@ -0,0 +1,11 @@
+package com.swa.oop2;
+
+public class Rectangle extends Shape {
+
+	
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return getHeight() * getWidth();
+	}
+
+}
---
+++ src/com/swa/oop2/Triangle.java
@@ -0,0 +1,11 @@
+package com.swa.oop2;
+
+public class Triangle extends Shape {
+
+	
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return (getHeight() * getWidth()) / 2;
+	}
+
+}
